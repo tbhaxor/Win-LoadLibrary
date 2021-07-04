98b38d71871a19c0c0b3ec31b0799b6b6684a4b8
Gurkirat Singh <tbhaxor@gmail.com>
2021-07-04 11:40:20 -0700

    commented code

--- DLLLoad/Source.cpp
+++ DLLLoad/Source.cpp
@@ -10,32 +10,42 @@ int _tmain(DWORD argc, LPTSTR* argv) {
 		return 1;
 	}
 
+	// convert TCHAR* to DWORD
 	DWORD dwX = _wtoi(argv[1]);
 	DWORD dwY = _wtoi(argv[2]);
-
+	
+	// load library and check if the handle is valid
+	// dlls are often called modules
 	HMODULE hDll = LoadLibrary(_T("MyDLL"));
 	if (!hDll || hDll == INVALID_HANDLE_VALUE) {
 		_tprintf(_T("unable to load libraray"));
 		return 1;
 	}
-
+		
+	// address of loaded dll
 	_tprintf(_T("library loaded at 0x%x\n"), hDll);
+
+
 	if (lstrcmp(_T("add"), argv[3]) == 0) {
+		// load AddFunc and casting to appropriate type
 		AddFunc Add = (AddFunc)GetProcAddress(hDll, "AddFunc");
 		if (!Add) _tprintf(_T("unable to load AddFunc\n"));
-		else _tprintf(_T("%d + %d = %d\n"), dwX, dwY, Add(dwX, dwY)); 
+		else _tprintf(_T("%d + %d = %d\n"), dwX, dwY, Add(dwX, dwY)); // calling function 
 	}
 	else if (lstrcmp(_T("div"), argv[3]) == 0) {
+		// load DivFunc and casting to appropriate type
 		DivFunc Div = (DivFunc)GetProcAddress(hDll, "DivFunc");
 		if (!Div) _tprintf(_T("unable to load DivFunc\n"));
 		else _tprintf(_T("%d / %d = %d\n"), dwX, dwY, Div(dwX, dwY));
 	}
 	else if (lstrcmp(_T("prod"), argv[3]) == 0) {
+		// load ProFunc and casting to appropriate type
 		ProFunc Pro = (ProFunc)GetProcAddress(hDll, "ProFunc");
 		if (!Pro) _tprintf(_T("unable to load DivFunc\n"));
 		else _tprintf(_T("%d * %d = %d\n"), dwX, dwY, Pro(dwX, dwY));
 	}
 	else if (lstrcmp(_T("sub"), argv[3]) == 0) {
+		// load SubFunc and casting to appropriate type
 		SubFunc Sub = (SubFunc)GetProcAddress(hDll, "ProFunc");
 		if (!Sub) _tprintf(_T("unable to load DivFunc\n"));
 		else _tprintf(_T("%d - %d = %d\n"), dwX, dwY, Sub(dwX, dwY));
@@ -44,5 +54,6 @@ int _tmain(DWORD argc, LPTSTR* argv) {
 		_tprintf(_T("invalid operation\n"));
 	}
 
+	
 	FreeLibrary(hDll);
 }
